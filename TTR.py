21afdecf019a220a16033f8c6bf54b0f5c30bab2
practicalRope <hkmelaa@hotmail.com>
2019-03-22 10:17:31 +0100

    add printmsg

--- TTR.py
+++ TTR.py
@@ -6,8 +6,10 @@ text = file.read().split()
 
 # types / unique words / set
 word_set = len(set(text))
+print(f"types: {word_set}")
 # tokens / number of words in the text / list
 word_list = len(text)
+print(f"tokens: {word_list}")
 
 # calculate the TTR 'type token ratio'
 ratio = (word_set / word_list) * 100
